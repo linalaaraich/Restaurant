74f16f5009d823347743e896f85c885b452bad60
Lina <laaraichlina@gmail.com>
2024-04-24 19:42:15 +0100

    finally

--- Restaurant.java
+++ Restaurant.java
@@ -18,8 +18,14 @@ public class Restaurant {
 
     public void customerSays(int tableId, String customerOrder){ //customerOrder : " Joe: Pizza"
         List<String> parts = Arrays.stream(customerOrder.split(":"))
-                                        .map(String::trim)
-                                        .collect(Collectors.toList());
+                .map(String::trim)
+                .collect(Collectors.toList());
+        int quantity = 1;
+
+        if (parts.size() >= 3 && parts.get(2).startsWith("for")) {
+            quantity = Integer.parseInt(parts.get(2).split("\\s+")[1]);
+            
+        }
         Order newOrder = new Order(parts.get(1), parts.get(0), tableId);
         orders.add(newOrder);
         for(Table table : tables){
@@ -37,9 +43,11 @@ public class Restaurant {
     }
 
     String createOrder(int tableID){
+        int missing = 10;
         String fullOrder = "";
         for (Table table : tables) {
             if(table.getTableID() == tableID) {
+                missing = table.getTableCapacity()-table.getTableOrders().size();
                 for (Order order : table.getTableOrders()) {
                     if (order.getTableID() == tableID) {
                         fullOrder += order.getOrder();
@@ -47,7 +55,11 @@ public class Restaurant {
                     }
                 }
             }
+            else return "ERROR";
         }
-        return fullOrder;
+        if(missing == 0) return fullOrder;
+        else return "Missing " + missing;
+
     }
+
 }
